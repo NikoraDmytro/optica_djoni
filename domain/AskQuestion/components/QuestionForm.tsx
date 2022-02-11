17bff17951f35bbdcdc8fd2c7ddc88a17c879c84
Dmytro <igorNikora456@gmail.com>
2022-02-11 18:20:33 +0200

    Added styles for the users questions form

--- domain/AskQuestion/components/QuestionForm.tsx
+++ domain/AskQuestion/components/QuestionForm.tsx
@@ -1,17 +1,43 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import { formValidationSchema } from "./utils/FormValidation";
+
+import styles from "./QuestionForm.module.scss";
+
+const initialValues = {
+  userName: "",
+  phoneNumber: "",
+};
+
 export const QuestionForm = () => {
-    return (
-        <form>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={formValidationSchema}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        console.log(values);
+
+        setSubmitting(false);
+        resetForm();
+      }}
+    >
+      <Form className={styles.questionForm}>
+        <div className={styles.inputGroup}>
           <label htmlFor="userName">Имя</label>
-          <input placeholder="Имя" name="userName" id="userName" />
+          <Field placeholder="Имя" name="userName" />
+          <ErrorMessage name="userName" className={styles.error} />
+        </div>
 
-          <label htmlFor="userPhoneNumber">Номер телефона</label>
-          <input
-            placeholder="+380"
-            name="userPhoneNumber"
-            id="userPhoneNumber"
-          />
+        <div className={styles.inputGroup}>
+          <label htmlFor="phoneNumber">Номер телефона</label>
+          <input placeholder="+380" name="phoneNumber" />
+          <ErrorMessage name="phoneNumber" className={styles.error} />
+        </div>
 
-          <button type="submit">Перезвоните мне</button>
-        </form>
-    )
-}
+        <button type="submit" className={styles.submitButton}>
+          Перезвоните мне
+        </button>
+      </Form>
+    </Formik>
+  );
+};
---
+++ domain/AskQuestion/components/utils/FormValidation.tsx
@@ -0,0 +1,9 @@
+import * as Yup from "yup";
+
+export const formValidationSchema = new Yup.ObjectSchema({
+  userName: Yup.string().matches(/[^0-9]+$/, "Do not use numbers!").required("Required!"),
+  phoneNumber: Yup.string()
+    .matches(/^\+\d{3}/, "Phone number must start with +380")
+    .matches(/^\+\d{3}\s\d{2}\s\d{3}\s\d{2}\s\d{2}/, "Please enter a valid phone number!")
+    .required("Required!"),
+});
